f0db38718b13af75e1380aa11cdc785848590a3f
YuriySamusew <6177385@gmail.com>
2022-10-16 18:57:02 +0300

    load more

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import './css/styles.css';
-import { fetchImg } from './js/fetchImages';
+import { PixabayAPI } from './js/PixabayAPI';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { createGallery } from './js/createGallery';
+import { refs } from './js/refs';
 
-const refs = {
-  inputRef: document.querySelector('#search-form'),
-  serchBtnRef: document.querySelector('.country-list'),
-  markupGalleryRef: document.querySelector('.gallery'),
+const pixabay = new PixabayAPI();
+// console.log(pixabay);
+
+const hendleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = event.currentTarget;
+  const searchingQuery = searchQuery.value.trim().toLowerCase();
+  if (!searchingQuery) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  pixabay.searchQuery = searchingQuery;
+
+  pixabay.getPhotos().then(({ hits }) => {
+    const markup = createGallery(hits);
+    // console.log(markup);
+    refs.markupGalleryRef.insertAdjacentHTML('beforeend', markup);
+  });
 };
 
-let importValue = '';
+const onLadMore = () => {
+  pixabay.incrementPage();
+  pixabay.getPhotos().then(({ hits }) => {
+    const markup = createGallery(hits);
+    // console.log(markup);
+    refs.markupGalleryRef.insertAdjacentHTML('beforeend', markup);
+  });
+};
 
-fetchImg(importValue).then(data => {
-  console.log(data);
-});
+refs.inputRef.addEventListener('submit', hendleSubmit);
+refs.ladMoreBtn.addEventListener('click', onLadMore);
